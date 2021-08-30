924fb199106d5ae0b050cd7a901e9b07a1dc039e
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-08-30 12:46:48 +0200

    fix integrationTest params

--- lib/src/generator_options.dart
+++ lib/src/generator_options.dart
@@ -6,21 +6,24 @@ class GeneratorOptions {
     String? testMethodName,
     List<String>? externalSteps,
     String? stepFolderName,
-    bool integrationTest = false
+    bool? integrationTest
   })  : stepFolder = stepFolderName ?? _stepFolderName,
         testMethodName = testMethodName ?? _defaultTestName,
-        externalSteps = externalSteps ?? const [];
+        externalSteps = externalSteps ?? const []
+        integrationTest =. integrationTest ?? false;
 
   factory GeneratorOptions.fromMap(Map<String, dynamic> json) =>
       GeneratorOptions(
         testMethodName: json['testMethodName'] as String?,
         externalSteps: (json['externalSteps'] as List?)?.cast<String>(),
         stepFolderName: json['stepFolderName'] as String?,
-        stepFolderName: json['integration_test'] as bool ?: false,
+        stepFolderName: json['integration_test'] as bool ,
 
       );
 
   final String stepFolder;
   final String testMethodName;
   final List<String> externalSteps;
+  final bool integrationTest;
+
 }
